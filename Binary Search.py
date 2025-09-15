3090ac3ec3752eab8e04bb6ac8d17095dd0e0432
Leon <166517099+scuranger0625@users.noreply.github.com>
2025-09-15 18:32:10 +0800

    Create Binary Search.py

---
+++ Binary Search.py
@@ -0,0 +1,9 @@
+def binary_search(nums, target):
+    lo, hi = 0, len(nums)
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if nums[mid] < target:
+            lo = mid + 1
+        else:
+            hi = mid
+    return lo  # 插入位置
