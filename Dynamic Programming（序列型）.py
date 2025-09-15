16338e17de8d7ad66d6cf68577534552b1d356da
Leon <166517099+scuranger0625@users.noreply.github.com>
2025-09-15 18:33:18 +0800

    Create Dynamic Programming（序列型）.py

---
+++ Dynamic Programming（序列型）.py
@@ -0,0 +1,7 @@
+def longest_increasing_subseq(nums):
+    dp = [1] * len(nums)
+    for i in range(len(nums)):
+        for j in range(i):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return max(dp, default=0)
